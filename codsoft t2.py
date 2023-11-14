58d1454da2753035ee4a81dba03dd4830b460893
Rajeshwari G <118655897+raji2005@users.noreply.github.com>
2023-11-14 14:19:04 +0530

    Add files via upload

---
+++ codsoft t2.py
@@ -0,0 +1,33 @@
+import operator
+
+add = operator.add
+subtract = operator.sub
+multiply = operator.mul
+divide = operator.truediv
+
+while True:
+    print("\nOptions:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Quit")
+
+    choice = input("Enter your choice (1/2/3/4/5): ")
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == "5":
+        break
+
+    if choice == "1":
+        print("Result:", add(num1, num2))
+    elif choice == "2":
+        print("Result:", subtract(num1, num2))
+    elif choice == "3":
+        print("Result:", multiply(num1, num2))
+    elif choice == "4":
+        print("Result:", divide(num1, num2))
+    else:
+        print("Invalid choice")
